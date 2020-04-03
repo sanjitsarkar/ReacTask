11df486ed093f2c3a25ad4dc30a1eedf9a557bbc
Sanjit Sarkar <ssanjit856@gmail.com>
2020-04-03 11:58:21 +0530

    Prepare for deploy to Heroku

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app =  express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-require('dotenv').config();
+const key = require('./config/key');
+const path = require('path');
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 const mongoose = require('mongoose');
-const URI = process.env.MD_URI;
-mongoose.connect(URI,{useNewUrlParser:true, useCreateIndex:true,useUnifiedTopology: true});
+
+mongoose.connect(key.URI,{useNewUrlParser:true, useCreateIndex:true,useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open',()=>
 {
@@ -26,6 +27,16 @@ const todosRouter = require('./routes/todos');
 
 
 app.use('/todos',todosRouter);
+
+
+if(process.env.NODE_ENV === 'production')
+{
+  app.use(express.static('../build'));
+  app.get('*',(req,res)=>
+  {
+res.sendFile(path.resolve(__dirname,'..','build','index.html'));
+  });
+}
 io.on('connection',(socket)=>
 {
   socket.on('add',(data)=>
