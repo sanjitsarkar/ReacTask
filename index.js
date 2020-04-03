786bdb4d2a8b9f31b05eaedaf4ede4cd82f99780
Sanjit Sarkar <ssanjit856@gmail.com>
2020-04-03 12:50:00 +0530

    Prepare for deploy to Heroku

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const todosRouter = require('./routes/todos');
 app.use('/todos',todosRouter);
 
 
-if(process.env.NODE_ENV !== 'production')
+if(process.env.NODE_ENV === 'production')
 {
   app.use(express.static('client/build'));
   console.log('Hello');
